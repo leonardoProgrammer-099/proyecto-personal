15cd7c963fe450959d79d54f385a545c25944109
Leonardo <darkargarcia85@gmail.com>
2024-02-03 21:54:37 -0600

    'oa'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndexComponent } from './pages/index/index.component';
 import { LogginComponent } from './pages/loggin/loggin.component';
 import { Router, RouterModule } from '@angular/router';
+import { CabeceroComponent } from './components/cabecero/cabecero.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IndexComponent,
-    LogginComponent
+    LogginComponent,
+    CabeceroComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
